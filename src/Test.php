6595e1967048690a895244942508e6e9f942f45d
Tomislav Trbic <31571445+trba1810@users.noreply.github.com>
2020-10-27 12:54:29 +0100

    init

---
+++ src/Test.php
@@ -0,0 +1,12 @@
+<?php
+namespace Pratice2020\Config;
+
+class Test
+{
+    public $x;
+
+    public function __construct()
+    {
+        $this->x = 4;
+    }
+}
---
+++ xfile.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'vendor/autoload.php';
+
+
+use Practice2020\Config;
+
+$var = new \Practice2020\Config\Test();
+
+echo $var->x;
