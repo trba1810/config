6010b0cbe726eed0ef317070e35bcc71cad42c63
Tomislav Trbic <31571445+trba1810@users.noreply.github.com>
2020-11-08 21:32:13 +0100

    improvements to adapter

--- src/Adapter/FileAdapter.php
+++ src/Adapter/FileAdapter.php
@@ -2,6 +2,7 @@
 namespace Practice2020\Config\Adapter;
 
 use AdapterInterface;
+// Jel sme i kako ako sme da koristim interface koji je u razlicitom fajlu od klase??
 
 class FileAdapter implements AdapterInterface
 {
@@ -10,34 +11,43 @@ class FileAdapter implements AdapterInterface
 
     public function __construct($path)
     {
-        // get extension od putanje
-        // if php,json ili xml 
-        // ako nije nista od toga greska
-        // rezultat treba da bude :
         $ext = pathinfo($path, PATHINFO_EXTENSION);
-        if($ext == 'json')
-        {
-            $this->data = $this->readJson($path);
-        }
-        elseif($ext == 'xml')
-        {
-            $this->data = $this->readXml($path);
-        }
-        else {
-            
-        }
         
+        switch($ext)
+        {
+            case "json":
+                $this->data = $this->readJson($path);
+            break;
+            case "xml":
+                $this->data = $this->readXml($path);
+            break;
+            case "php":
+                // nesto
+            break;
+            default:
+                throw new Exception('wrong extension');
+        } 
     }
 
     private function readXml($path)
     {
+        $file = file_get_contents($path, true);
+        $new = simplexml_load_string($file); 
+        $con = json_encode($new); 
+        $newArr = json_decode($con, true);
 
+        // jel ovako trebalo xml to object,object to json,json to array???
     }
 
     private function readJson($path)
     {
         //procitati kontent fajla
         //pokusati json decode i return
+        // Jel ovo trebalo??
+
+        $file = file_get_contents($path, true);
+        return json_decode($file);
+
     }
 
     public function getData($data)
--- xfile.php
+++ xfile.php
@@ -1,8 +1,12 @@
 <?php
 require_once 'vendor/autoload.php';
 
+
 $var = new \Practice2020\Config\Test();
+$var1 = new \Practice2020\Config\Adapter\FileAdapter();
 
 echo $var->x;
 
 echo 'n';
+
+echo $ext;
